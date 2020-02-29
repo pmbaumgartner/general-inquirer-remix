9452b23c2ffaaa59f2b53170d82acee61848ec4d
Peter Baumgartner <pb@example.com>
2020-02-29 18:18:40 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="general-inquirer-remix",
     version="0.1",
-    packages=find_packages(),
+    packages=["general_inquirer"],
     description="This is a version of the General Inquirer (GI) that operates as a spaCy extension.",
     author="pmbaumgartner",
     url="https://github.com/pmbaumgartner/general-inquirer-remix",
     install_requires=["spacy"],
     python_requires=">=3.6",
 )
-
